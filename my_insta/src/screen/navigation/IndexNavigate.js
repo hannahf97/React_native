ae684abbb5415d4c6042afbeff3435f5480fede5
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-06-22 09:34:36 +0900

    [Add] indexNavigate postadd

--- my_insta/src/screen/navigation/IndexNavigate.js
+++ my_insta/src/screen/navigation/IndexNavigate.js
@@ -1,11 +1,41 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login, loginCheck } from "../../store/users";
+import PostUpdateScreen from "../PostUpdateScreen";
 import BottomTabScreen from "./BottomTabScreen";
-import LoginNavigate from "./LoginNavigate";
-import { useSelector } from "react-redux";
+import LoginNavigate from "./LoginNavigete";
 
 const IndexNavigate = () => {
   const isLogin = useSelector((state) => state.users.isLogin);
-  return isLogin ? (
-    <BottomTabScreen></BottomTabScreen>
+  const dispatch = useDispatch();
+  const reLogin = async () => {
+    const user = await AsyncStorage.getItem("user");
+    dispatch(login(JSON.parse(user)));
+  };
+  useEffect(() => {
+    reLogin();
+  }, []);
+  const Stack = createStackNavigator();
+  return isLogin ? ( //
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="home"
+          component={BottomTabScreen} //
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="PostAdd"
+          component={PostUpdateScreen}
+          options={{
+            headerTitle: "글 쓰기",
+          }}
+        ></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   ) : (
     <LoginNavigate></LoginNavigate>
   );
